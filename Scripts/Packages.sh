e9a7733f68f5ca1f41f8a18c855d9e2f88593e9f
bbvdf <bbvdf@users.noreply.github.com>
2026-02-20 09:34:30 +0800

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -69,7 +69,7 @@ UPDATE_PACKAGE "luci-app-daed" "QiuSimons/luci-app-daed" "master"
 UPDATE_PACKAGE "aurora" "eamonxg/luci-theme-aurora" "master"
 UPDATE_PACKAGE "aurora-config" "eamonxg/luci-app-aurora-config" "master"
 #UPDATE_PACKAGE "adguardhome" "kenzok8/small-package" "main" "pkg" "luci-app-adguardhome"
-UPDATE_PACKAGE "adguardhome" "w9315273/luci-app-adguardhome" "master"
+UPDATE_PACKAGE "luci-app-adguardhome" "OneNAS-space/luci-app-adguardhome" "master"
 
 #更新软件包版本
 UPDATE_VERSION() {

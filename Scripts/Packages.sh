3b101dc7f5fdee7840a939522734b41021c19ac5
bbvdf <bbvdf@users.noreply.github.com>
2026-03-09 09:17:11 +0800

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -78,9 +78,8 @@ UPDATE_PACKAGE "aurora-config" "eamonxg/luci-app-aurora-config" "master"
 
 # adguardhome包
 
-#UPDATE_PACKAGE "adguardhome" "xiaoxiao29/luci-app-adguardhome" "master" "pkg"
-#UPDATE_PACKAGE "luci-app-adguardhome" "xiaoxiao29/luci-app-adguardhome" "master" "pkg"
-UPDATE_PACKAGE "adguardhome" "xiaoxiao29/luci-app-adguardhome" "master" "pkg"
+UPDATE_PACKAGE "adguardhome" "kenzok8/openwrt-packages" "master" "pkg"
+UPDATE_PACKAGE "luci-app-adguardhome" "kenzok8/openwrt-packages" "master" "pkg"
 
 #UPDATE_PACKAGE "luci-app-adguardhome" "OneNAS-space/luci-app-adguardhome" "master"
 #UPDATE_PACKAGE "luci-app-adguardhome" "w9315273/luci-app-adguardhome" "master"

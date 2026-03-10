09f467aded0e1c5a3f0a66d87adc654df8c3eccd
bbvdf <bbvdf@users.noreply.github.com>
2026-03-10 11:56:58 +0800

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -82,7 +82,7 @@ UPDATE_PACKAGE "adguardhome" "kenzok8/openwrt-packages" "master" "pkg"
 #UPDATE_PACKAGE "luci-app-adguardhome" "kenzok8/openwrt-packages" "master" "pkg"
 
 rm -rf luci-app-adguardhome
-UPDATE_PACKAGE "luci-app-adguardhome" "w9315273/luci-app-adguardhome" "master"
+git clone --depth 1 https://github.com/w9315273/luci-app-adguardhome package/luci-app-adguardhome
 
 #UPDATE_PACKAGE "luci-app-adguardhome" "OneNAS-space/luci-app-adguardhome" "master"
 #UPDATE_PACKAGE "luci-app-adguardhome" "stevenjoezhang/luci-app-adguardhome" "dev"
